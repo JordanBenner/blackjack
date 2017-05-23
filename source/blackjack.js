59b2cd130872d7ebede8fff0d9de333c6985f2e6
Jordan Benner <jordanbenner@outlook.com>
2017-05-23 14:41:19 -0500

    Added stand button

--- source/blackjack.js
+++ source/blackjack.js
@@ -1,45 +1,48 @@
-
-
-var numbers = ['Ace', '2','3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+var numbers = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 var suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
-var aceOfSpades = {point: 1, suit: 'spades'};
-var kingOfHearts = {point: 13, suit: 'hearts' };
-var queenOfDimonds = {point: 12, suit: 'dimonds'};
-var jackOfClubs = {point: 11, suit: 'clubs'};
-var deck = [
-  {}
-];
+var deck = [];
 
-for (let i=0; i < suits.length; i++) {
-  for (let j=0; j < numbers.length; j++) {
+for (let i = 0; i < suits.length; i++) {
+  for (let j = 0; j < numbers.length; j++) {
     //add a card to the deck
-    deck.push({suit: suits[i], points: j + 1, card_number: numbers[j]});
+    deck.push({
+      suit: suits[i],
+      points: j + 1,
+      card_number: numbers[j]
+    });
   }
 }
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
 }
 
 deck = shuffleArray(deck);
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('#deal-button').click(function(){
+  $('#deal-button').click(function() {
     var card = deck.pop();
     $('#dealer-hand').append(`<img src='images/${card.card_number}_of_${card.suit}.png'>`);
+
+    // console.log('#player-hand', playerHand);
+    // console.log('#dealer-hand', dealerHand);
   });
 
-  $('#hit-button').click(function(){
+  $('#hit-button').click(function() {
     var card = deck.pop();
     $('#player-hand').append(`<img src='images/${card.card_number}_of_${card.suit}.png'>`);
   });
 
+  $('#stand-button').click(function() {
 
   });
+
+
+});
